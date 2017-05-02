2996e77f93f31b697b07bcfbe9a0b58285b32a52
Dark(김성진) <ekdxhrl@coupang.com>
2017-05-02 14:47:28 +0900

    Initialize

---
+++ src/main/java/com/dark/StudyProjectApplication.java
@@ -0,0 +1,12 @@
+package com.dark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyProjectApplication.class, args);
+	}
+}
